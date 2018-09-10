22e77189142cf6a152e7b100815c7395c8320725
Natalia Favero <nataliafavero@Natalias-MacBook-Pro.local>
2018-09-10 17:41:27 -0300

    Models

--- TesteiOs/TesteiOs/Models/Cell.swift
+++ TesteiOs/TesteiOs/Models/Cell.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+struct Cell:Equatable {
+    var id: Int
+    var type: Type
+    var message: String
+    var typefield: TypeField
+    var hidden: Bool
+    var topSpacing: Int
+    var show: Int
+    var required: Bool
+}
+
+enum Type: Int {
+    case field = 1
+    case text = 2
+    case image = 3
+    case checkbox = 4
+    case send = 5
+}
+
+enum TypeField:Int{
+    case text = 1
+    case telNumber = 2
+    case email = 3
+}
+
+func ==(lhs: Cell, rhs: Cell) -> Bool
+{
+    return lhs.id == rhs.id
+        && lhs.type == rhs.type
+        && lhs.message == rhs.message
+        && lhs.typefield == rhs.typefield
+        && lhs.hidden == rhs.hidden
+        && lhs.topSpacing == rhs.topSpacing
+        && lhs.show == rhs.show
+        && lhs.required == rhs.required
+}
--- TesteiOs/TesteiOs/Models/Screen.swift
+++ TesteiOs/TesteiOs/Models/Screen.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+
+struct Screen: Equatable {
+    var title: String
+    var fundName: String
+    var whatIs: String
+    var definition: String
+    var riskTitle: String
+    var risk: Int
+    var infoTitle: String
+    var moreInfo: MoreInfo
+    var info: [Info]
+    var downInfo: [Info]
+}
+
+func ==(lhs: Screen, rhs: Screen) -> Bool
+{
+    return lhs.title == rhs.title
+        && lhs.fundName == rhs.fundName
+        && lhs.whatIs == rhs.whatIs
+        && lhs.definition == rhs.definition
+        && lhs.riskTitle == rhs.riskTitle
+        && lhs.risk == rhs.risk
+        && lhs.infoTitle == rhs.infoTitle
+        && lhs.moreInfo == rhs.moreInfo
+        && lhs.info == rhs.info
+        && lhs.downInfo == rhs.downInfo
+}
+
+struct MoreInfo: Equatable {
+    var month: MoreInfoDetail
+    var year: MoreInfoDetail
+    var twelveMonths: MoreInfoDetail
+}
+
+func ==(lhs: MoreInfo, rhs: MoreInfo) -> Bool
+{
+    return lhs.month == rhs.month
+        && lhs.year == rhs.year
+        && lhs.twelveMonths == rhs.twelveMonths
+}
+
+struct MoreInfoDetail: Equatable {
+    var fund: Float
+    var CDI: Float
+}
+
+func ==(lhs: MoreInfoDetail, rhs: MoreInfoDetail) -> Bool
+{
+    return lhs.fund == rhs.fund
+        && lhs.CDI == rhs.CDI
+}
+
+struct Info: Equatable {
+    var name: String
+    var data: String
+}
+
+func ==(lhs: Info, rhs: Info) -> Bool
+{
+    return lhs.name == rhs.name
+        && lhs.data == rhs.data
+}
+
